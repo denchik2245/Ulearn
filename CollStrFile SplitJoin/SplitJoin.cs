ae4d539469c97db1f6afe806dcead27a1ee322cf
denchik 1337 <flarich2281337@gmail.com>
2023-10-31 18:10:48 +0300

    Add task "SplitJoin"

---
+++ CollStrFile SplitJoin/SplitJoin.cs
@@ -0,0 +1,31 @@
+﻿public class BenfordsLaw
+{
+    public static void Main()
+    {
+        // Пример текста с различными разделителями для теста.
+        string sampleText = "Country:USA-Population:328M\nCountry:India;Population:1.3B\nCountry:China,Population:1.4B";
+    
+        string result = ReplaceIncorrectSeparators(sampleText);
+        Console.WriteLine(result);
+    }
+
+    public static string ReplaceIncorrectSeparators(string text)
+    {
+        // Разбиваем входной текст на строки по символам новой строки ('\n' и '\r').
+        string[] lines = text.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        
+        string result = ""; // Создаем пустую строку для хранения результата.
+        
+        foreach (string line in lines) // Проходим по каждой строке из разделенного текста.
+        {
+            // Разбиваем строку на части, используя разные разделители.
+            string[] parts = line.Split(new char[] { ' ', ':', ';', '-', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Если в строке найдено 4 или больше частей, добавляем их в результирующую строку, разделяя символом табуляции.
+            if (parts.Length >= 4)
+                result += parts[0] + "\t" + parts[1] + "\t" + parts[2] + "\t" + parts[3] + "\n";
+        }
+        return result;
+    }
+
+}
